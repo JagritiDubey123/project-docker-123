550ec778a8f295b34603600f0b31904af1aadebe
Jagriti <129485043+JagritiDubey123@users.noreply.github.com>
2024-04-29 11:06:16 +0530

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: "192.168.49.2:32000",
+  host: "mysql",
   user: "root",
   password: "jagriti@123",
   port: "3306",
