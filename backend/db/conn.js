149573938a643f4cde9f3b8670cd157d7dc612d7
Jagriti <129485043+JagritiDubey123@users.noreply.github.com>
2024-04-30 14:14:53 +0530

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,19 +1,43 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 
+
+// // Create a connection to the database
+// const connection = mysql.createConnection({
+//   host: "mysql",
+//   user: "root",
+//   password: "jagriti@123",
+//   port: "3306",
+//   database: "employees_db"
+// });
+
+// // open the MySQL connection
+// connection.connect(error => {
+//   if (error) throw error;
+//   console.log("Successfully connected to the MYSQL database.");
+// });
+
+// module.exports = connection;
+
+
+
+const mysql = require("mysql2");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
   host: "mysql",
   user: "root",
   password: "jagriti@123",
-  port: "3306",
   database: "employees_db"
 });
 
 // open the MySQL connection
 connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the MYSQL database.");
+  if (error) {
+    console.error("Error connecting to MySQL:", error);
+    process.exit(1); // Exit the application if unable to connect
+  }
+  console.log("Successfully connected to the MySQL database.");
 });
 
 module.exports = connection;
+
